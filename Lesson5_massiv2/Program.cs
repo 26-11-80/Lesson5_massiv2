7a6cf4c1f6fb91b12aa99d85a2f4f9c9d3216494
Гульназик <@LAPTOP-58TJUTTA>
2021-11-27 18:53:36 +0500

    Добавьте файлы проекта.

---
+++ Lesson5_massiv2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5_massiv2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размерность массива");
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] array = new int[size, size];
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    bool isZero = (i + j) % 2 != 0;
+                    if (isZero)
+                    {
+                        array[i, j] = 0;
+                    }
+                    else
+                    {
+                        array[i, j] = 1;
+                    }
+
+
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("{0,3}", array[i, j]);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
